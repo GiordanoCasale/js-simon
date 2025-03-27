dc185817c392e0ab6091d8c0f5beb3b1a0399c44
GiordanoCasale <giordanocasale8@gmail.com>
2025-03-27 15:49:30 +0100

    [UPD] Gnerate random number done

--- js/script.js
+++ js/script.js
@@ -21,10 +21,20 @@ function generateNumbers(min,max){
     const numbers = []
     const arrayNumbers = 5
 
-    for(let i=0; i<numeriArray; i++){
+    for(let i=0; i<arrayNumbers; i++){
         let random = Math.floor(Math.random() * (max - min + 1) + min);
         numbers.push(random);
     }
 
     return numbers
+}
+
+const randomNumbers = generateNumbers(1,50);
+console.log(randomNumbers);
+
+//creiamo una lista in cui inserire i numeri randomici
+for(let i = 0; i<randomNumbers.length; i++) {
+    const item = document.createElement('li');
+    item.innerText = randomNumbers[i];
+    numberList.appendChild(item);
 }
